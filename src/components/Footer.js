867a26e4a2902e3bb4a706c8661df63af563745a
Cynthia Mengyuan Li <cynthiamengyuanli@gmail.com>
2024-02-26 16:53:26 +0100

    updated layout for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,28 @@
 import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPepperHot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPepperHot } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
-  return (
-
-      <footer class="footer">
-        <div class="footer-addr">
-          <FontAwesomeIcon className="icon" icon={faPepperHot} />              
-          <a class="navbar-brand" href="/">
-              Spice IT Global
-          </a>
-          <a class="footer-btn" href="mailto:spiceitglobal@gmail.com">Email Us</a>   
-        </div>
-        <address>Gothenburg, Sweden</address>
-        <div class="legal">
-          <p>&copy; 2024 Spice IT Global. All rights reserved.</p>
-        </div>
-      </footer>
-
-  )
+	return (
+		<footer class="footer">
+			<div class="footer-content">
+				<div class="icon-brand">
+					<FontAwesomeIcon className="icon" icon={faPepperHot} />
+					<a class="navbar-brand" href="/">
+						Spice IT Global
+					</a>
+				</div>
+				<a class="footer-btn" href="mailto:spiceitglobal@gmail.com">
+					Email Us
+				</a>
+				<address>Gothenburg, Sweden</address>
+				<div class="legal">
+					<p>&copy; 2024 Spice IT Global. All rights reserved.</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
 
 export { Footer };
